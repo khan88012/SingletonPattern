c035686aab368c9480bec1aac27eabbf683649c0
Md Asif Khan <asif.k@technovert.com>
2024-01-09 13:06:54 +0530

    eager initialization for singleton object

---
+++ Burger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal class Burger
+    {
+        //eager singleton object creation. 
+        private static Burger burger = new Burger();    
+
+        private Burger() { }
+
+        public static Burger GetBurgerObject()
+        {
+            return burger;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ Console.WriteLine("Singleton Desgin Pattern Example!");
 //Below line wont work as Pizza class constructor is private
 //Pizza pizza = new Pizza();
 
-Pizza pizza = Pizza.GetPizzaObject();
+//Pizza pizza = Pizza.GetPizzaObject();
 //every object as an unique hashcode since we are claiming that we will get single object in singleton pattern we should get both the hashcode similar
-Console.WriteLine(pizza.GetHashCode());
+//Console.WriteLine(pizza.GetHashCode());
 
 
-Pizza pizza1 = Pizza.GetPizzaObject();
-Console.WriteLine(pizza1.GetHashCode());
+//Pizza pizza1 = Pizza.GetPizzaObject();
+//Console.WriteLine(pizza1.GetHashCode());
+
+
+
+Burger burger1 = Burger.GetBurgerObject();
+Burger burger2 =  Burger.GetBurgerObject();
+
+if(burger1.GetHashCode() == burger2.GetHashCode())
+{
+    Console.WriteLine("Same Burger object is returned");
+}
